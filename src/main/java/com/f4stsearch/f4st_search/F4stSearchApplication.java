ed8372ea00620c81d782b32946a396ab5d8554c8
Ryan Lucas <ryanlucaspatron@gmail.com>
2025-08-30 21:26:42 -0300

    Project Creation

---
+++ src/main/java/com/f4stsearch/f4st_search/F4stSearchApplication.java
@@ -0,0 +1,13 @@
+package com.f4stsearch.f4st_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F4stSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F4stSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f4stsearch/f4st_search/F4stSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f4stsearch.f4st_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F4stSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
