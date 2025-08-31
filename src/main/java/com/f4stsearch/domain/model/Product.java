438631a6a10223999b1b45b41682e8c1d424afed
Ryan Lucas <ryanlucaspatron@gmail.com>
2025-08-31 18:18:52 -0300

    Feat: update of Product and pom.xml

--- src/main/java/com/f4stsearch/domain/model/Product.java
+++ src/main/java/com/f4stsearch/domain/model/Product.java
@@ -2,11 +2,22 @@ package com.f4stsearch.domain.model;
 
 import lombok.*;
 
-@Data
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class Product {
     private String id;
-    private String normalizedTitle;
+    private String title;
+    private Double price;
+    private String permalink;
+
+    public Product(String id, String title, Double price, String permalink) {
+        this.id = id;
+        this.title = title;
+        this.permalink = permalink;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
 }
