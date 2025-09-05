b7d78277b1d95794b5bab8cd8edc8821eb259b24
Ryan Lucas <ryanlucaspatron@gmail.com>
2025-09-05 19:42:01 -0300

    Feat: new Product class creation

--- src/main/java/com/f4stsearch/domain/model/Product.java
+++ src/main/java/com/f4stsearch/domain/model/Product.java
@@ -1,23 +1,96 @@
 package com.f4stsearch.domain.model;
 
-import lombok.*;
 
-@NoArgsConstructor
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
-    private String id;
-    private String title;
-    private Double price;
-    private String permalink;
 
-    public Product(String id, String title, Double price, String permalink) {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Double price;
+    private String description;
+    private String category;
+    private String image;
+    private Rating rating;
+
+    public Product(Long id, String title, String description, Double price, String category, String image, Rating rating) {
         this.id = id;
         this.title = title;
-        this.permalink = permalink;
+        this.description = description;
         this.price = price;
+        this.category = category;
+        this.image = image;
+        this.rating = rating;
+    }
+
+    public Product() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public double getRateValue() {
+        return this.rating != null ? this.rating.getRate() : 0.0;
+    }
 }
